4cbdee56056e856221ad83a82017817a675dee06
Xin-hai <aa1940358283@163.com>
2023-07-01 22:24:49 +0800

    提交第一版极简导航

---
+++ dist/main.2f0dae1b.js
@@ -0,0 +1,142 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"epB2":[function(require,module,exports) {
+var $ = function $(selector) {
+    return document.querySelector(selector);
+};
+var $$ = function $$(selector) {
+    return document.querySelectorAll(selector);
+};
+
+var currentUrl = 'https://www.baidu.com/s?wd=';
+
+$$('.search-panel >li').forEach(function ($li) {
+    $li.onclick = function () {
+        $$('.search-panel >li').forEach(function ($node) {
+            return $node.classList.remove('active');
+        });
+        this.classList.add('active');
+        var placeholder = this.dataset.holder;
+        $('.search-input').setAttribute('placeholder', placeholder);
+        currentUrl = this.dataset.url;
+    };
+});
+
+$('.btn').onclick = function () {
+    var $link = document.createElement('a');
+    $link.setAttribute('target', '_blank');
+    $link.setAttribute('href', currentUrl + $('.search-input').value);
+    $link.click();
+    // location.href = currentUrl + $('.search-input').value
+};
+
+document.addEventListener('keydown', function (e) {
+    if (e.key == 'Enter') {
+        location.href = currentUrl + $('.search-input').value;
+    }
+});
+},{}]},{},["epB2"], null)
+//# sourceMappingURL=main.2f0dae1b.map
---
+++ readme.md
@@ -0,0 +1,8 @@
+# 极简搜素
+# 预览链接： []()
+# yarn init -y 命令
+初始化package.json
+# yarn  build 命令
+```angular2html
+ rm -rf dist && parcel build src/index.html --no-minify --public-url ./
+
